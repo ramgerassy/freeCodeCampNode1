37dad3c941eb855525193d033b7d2780fcb99fcf
Ram Gerassy <ram.gerassy@gmail.com>
2022-11-22 21:27:55 +0200

    updated sendFile string

--- myApp.js
+++ myApp.js
@@ -1,10 +1,11 @@
 let express = require('express');
 let app = express();
 
-
+console.log(__dirname + '\\views\\index.html')
 app.get("/",(req,res)=>{
     //res.send("Hello Express")
-    res.sendfile('${__dirname}views/index.html')
+    res.sendfile(__dirname + '\\views\\index.html')
+    
 })
 
 
