c1a13c825ec42065f1e57eef19967dda4babcfd6
Ram Gerassy <ram.gerassy@gmail.com>
2022-11-22 21:28:05 +0200

    f

--- myApp.js
+++ myApp.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let app = express();
 
-console.log(__dirname + '\\views\\index.html')
 app.get("/",(req,res)=>{
     //res.send("Hello Express")
     res.sendfile(__dirname + '\\views\\index.html')
